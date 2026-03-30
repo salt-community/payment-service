f3ac3bf08139a13b79d5f6e516ca5044405e73f6
Johan Janérs <johanjaners@gmail.com>
2026-03-30 11:09:13 +0200

    Remove IProcessedEventRepository interface as it is no longer needed

--- src/PaymentService.Application/Interfaces/IProcessedEventRepository.cs
+++
@@ -1,7 +0,0 @@
-namespace PaymentService.Application.Interfaces;
-
-public interface IProcessedEventRepository
-{
-    Task<bool> HasProcessedEventAsync(Guid eventId, CancellationToken ct = default);
-    Task MarkAsProcessedAsync(Guid eventId, CancellationToken ct = default);
-}
