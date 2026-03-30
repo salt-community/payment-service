b865c7d4ce7d723cba4f230d38e6b0791dd2edd4
Johan Janérs <johanjaners@gmail.com>
2026-03-30 11:29:23 +0200

    Remove EventId property from BookingCreatedEvent class

--- src/PaymentService.Application/Events/BookingCreatedEvent.cs
+++ src/PaymentService.Application/Events/BookingCreatedEvent.cs
@@ -2,7 +2,6 @@ namespace PaymentService.Application.Events;
 
 public class BookingCreatedEvent
 {
-    public Guid EventId { get; set; }
     public Guid BookingId { get; set; }
     public string CustomerName { get; set; } = string.Empty;
     public string CustomerEmail { get; set; } = string.Empty;
